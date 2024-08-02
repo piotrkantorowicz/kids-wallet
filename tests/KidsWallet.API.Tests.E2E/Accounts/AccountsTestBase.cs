0ec1ebdafd94f50b1af2d5931891c2de7f9a7c0d
Piotr Kantorowicz <87525551+piotrkantorowicz@users.noreply.github.com>
2024-08-02 07:32:02 +0200

    Move tests outside of source dir (#15)

---
+++ tests/KidsWallet.API.Tests.E2E/Accounts/AccountsTestBase.cs
@@ -0,0 +1,33 @@
+﻿using Bogus.Extensions;
+
+using KidsWallet.API.Proxy.Requests.Accounts.POST.CreateKidAccount.Request;
+using KidsWallet.API.Tests.E2E.Wallets;
+
+namespace KidsWallet.API.Tests.E2E.Accounts;
+
+internal abstract class AccountsTestBase : WalletsTestBase
+{
+    protected Guid _accountId;
+
+    protected async Task CreateAccount()
+    {
+        // Arrange
+        await CreateWallet();
+
+        await WebApp.Host.Scenario(configure: x =>
+        {
+            _accountId = _faker.Random.Guid();
+            string? accountName = _faker.Random.String2(length: 10);
+            decimal balance = _faker.Random.Decimal2(min: 150, max: 1000);
+
+            CreateKidAccountRequest createRequest = new(KidAccountId: _accountId, KidWalletId: _walletId,
+                Name: accountName, Balance: balance);
+
+            // Act
+            x.Post.Json(input: createRequest).ToUrl(url: "/v1/accounts");
+
+            // Assert
+            x.StatusCodeShouldBe(statusCode: 200);
+        });
+    }
+}
